6140c78fb40ce00bdce36a92be146280cf43a376
hossamgasser <hossgasser@gmail.com>
2026-03-04 21:12:11 -0500

    Login UI

--- app/src/main/java/com/example/popin/MainActivity.java
+++ app/src/main/java/com/example/popin/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,5 +23,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
